2c509a2a9025deebe06268bf09d32c2c5e3e0ef8
Romina <rominahelou@gmail.com>
2025-08-20 22:13:23 +0200

    Configuring the router@

---
+++ src/components/Watchlist.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+
+export default function Watchlist() {
+    return(
+        <div>
+            <h1 className="title">Your watchlist</h1>
+        </div>
+    )
+}
