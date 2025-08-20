98876d6a9182701ff2827b66ef989938f24805d1
Romina <rominahelou@gmail.com>
2025-08-20 14:08:07 +0200

    useState in react

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -7,12 +7,27 @@ export default function MoviesGrid(){
 
     useEffect(() => {
 
-        const m = ["a", "b", "c"]
-        
-        setMovies(m);
+    fetch('movies.json')
+    .then(response => response.json())
+    .then(data => setMovies(data))
+
     },[])
 
     return(
-        <div> {movies.length} </div>
+        <div className="movies-grid"> 
+        {
+            movies.map(movie => (
+                <div key={movie.id} className="movie-card">
+                    <img src={`images/${movie.image}`} alt={movie.title} />
+                <div className="movie-card-info">
+                    <h3 className="movie-card-title"> {movie.title} </h3>
+                    <p className="movie-card-genre"> {movie.genre} </p>
+                    <p className="movie-card-rating"> {movie.rating} </p>
+
+                </div>
+                </div>
+            ))
+        }
+        </div>
     )
 }
